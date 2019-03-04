023b3bde30135589049e1fe74b516f09d297ce66
shaileshg <shailesh.gawade@xpressbees.com>
2019-03-04 19:00:33 +0530

    SVCClosure validation
    Summary: SVCClosure validation
    
    Test Plan: tested on browser
    
    Reviewers: shaileshg
    
    Subscribers: priyankapawar
    
    Differential Revision: https://phab.xbees.in/D8077

--- src/views/components/svcclosurep2p/svcclosurep2p.component.js
+++ src/views/components/svcclosurep2p/svcclosurep2p.component.js
@@ -11,6 +11,11 @@ export default {
 
   data() {
     return {
+      DepositDate:"",
+      dep_amount:"",
+      DepositType:"",
+      BankName:"",
+      TransactionID:""
     }
   },
 
@@ -22,20 +27,36 @@ export default {
   },
 
   methods: {
+    onSubmit: function(event) {
+      this.$validator.validateAll().then((result) => {
+         if(result){
+          if((this.tot_amt != '0' && this.tot_amt != this.Deposit_Amount)||!this.tot_amt){
+            let error = document.getElementById("d_a");
+             error.innerHTML = " 	Enter Denomination details. Amount mismatches";
+             error.style.display = "block";
+          }else{
+            let error = document.getElementById("d_a");
+             error.innerHTML = "The Deposit_Amount Field is Required";
+             error.style.display = "None";
+          }
+        }
+          // event.target.reset();
+      }).catch(() => {
+        console.log('errors exist', this.errors)
+      });
+    },
 
     //function is used for calculate notes amount
     notesCount(event){
-console.log("event",event);
-      if(event.srcElement.value){
-        document.getElementById("mo"+event.srcElement.id).value = event.srcElement.id * event.srcElement.value;
-
+      if(event.target.id){
+        document.getElementById("mo"+event.target.id).value = event.target.id * event.target.value;
         var arr = document.getElementsByName('note_amt');
-        var tot=0;
+        this.tot_amt = 0;
         for(var i=0;i<arr.length;i++){
             if(parseInt(arr[i].value))
-                tot += parseInt(arr[i].value);
+                this.tot_amt += parseInt(arr[i].value);
         }
-        document.getElementById('tot_amt').value = tot;
+        document.getElementById('tot_amt').value = this.tot_amt;
       }
     },
 
