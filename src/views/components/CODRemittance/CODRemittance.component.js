199769a42c849350169ba4f18b21535625c8aa4d
shaileshg <shailesh.gawade@xpressbees.com>
2019-03-18 11:18:16 +0530

    Commit COD Remittance html
    Summary: CDO Remittance html
    
    Test Plan: Unit Test
    
    Reviewers: shaileshg
    
    Differential Revision: https://phab.xbees.in/D8538

--- src/views/components/CODRemittance/CODRemittance.component.js
+++ src/views/components/CODRemittance/CODRemittance.component.js
@@ -1,16 +1,27 @@
 import apiUrl from '../../../constants'
 import axios from 'axios'
-import {
-  Validator
-} from 'vee-validate'
+import CryptoJS from 'crypto-js';
+import { Validator } from 'vee-validate'
+import Paginate from 'vuejs-paginate'
+import VueElementLoading from 'vue-element-loading';
+import Multiselect from 'vue-multiselect'
+import 'vue-multiselect/dist/vue-multiselect.min.css';
 
 export default {
   name: 'mapbankhub',
-  components: {},
-  props: [],
+  components: {
+      Paginate,
+      VueElementLoading,
+      Multiselect
+  },
 
   data() {
     return {
+      localhubid: 0,
+      localhubname: '',
+      day: '',
+      ClientMasterID: '',
+      ClientList: []
     }
   },
 
@@ -19,14 +30,57 @@ export default {
   },
 
   mounted() {
+    var userdetailEncrypt = window.localStorage.getItem('accessuserdata')
+    var bytes             = CryptoJS.AES.decrypt(userdetailEncrypt.toString(), 'Key');
+    var plaintext         = bytes.toString(CryptoJS.enc.Utf8);
+    var userdetail        = JSON.parse(plaintext);
+    this.localuserid      = userdetail.userid;
+
+    var hubdetailEncrypt  = window.localStorage.getItem('accesshubdata')
+    var bytes             = CryptoJS.AES.decrypt(hubdetailEncrypt.toString(), 'Key');
+    var plaintext         = bytes.toString(CryptoJS.enc.Utf8);
+    var hubdetail         = JSON.parse(plaintext);
+    this.localhubid       = hubdetail[0].HubID;
+    this.localhubname     = hubdetail[0].HubName;
+
+    var userToken = window.localStorage.getItem('accessuserToken')
+    this.myStr = userToken.replace(/"/g, '');
+
+    this.GetClientData();
   },
 
   methods: {
+    multiple(){
+      return true;
+    },
+    GetClientData() {
+
+      axios({
+        method: 'GET',
+        url: apiUrl.api_url + 'external/getclientlist',
+        data: {},
+        headers: {
+          'Authorization': 'Bearer '+this.myStr
+        }
+      }).then(result => {
+        this.ClientList = result.data.clients.data;
+      }, error => {
+        console.error(error)
+      })
+    },
+
+    //to get pagination
+    getPaginationData(pageNum) {
+        this.pageno = (pageNum - 1) * 10
+        this.GetSVCledgerData()
+    },
 
     onSubmit: function(event) {
-      this.$validator.validateAll().then(() => {
-        console.log('form is valid', this.model)
-        event.target.reset();
+      this.$validator.validateAll().then((result) => {
+        if(result){
+           //this.saveSvcClosure(event);
+           event.target.reset();
+         }
       }).catch(() => {
         console.log('errors exist', this.errors)
       });
