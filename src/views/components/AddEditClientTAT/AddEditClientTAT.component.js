2a8aea6d9bb614121b2e83f905827fa6abc88126
saurabh.jaiswal <saurabh.jaiswal@xpressbees.com>
2019-03-30 18:24:35 +0530

    Commit Client COD Remittance Add and API functionality
    Summary: Commit Client COD Remittance Add and API functionality
    
    Test Plan: Unit Test
    
    Reviewers: shaileshg
    
    Maniphest Tasks: T21401
    
    Differential Revision: https://phab.xbees.in/D9001

--- src/views/components/AddEditClientTAT/AddEditClientTAT.component.js
+++ src/views/components/AddEditClientTAT/AddEditClientTAT.component.js
@@ -5,10 +5,11 @@ import {
 } from 'vee-validate'
 import Multiselect from 'vue-multiselect'
 import 'vue-multiselect/dist/vue-multiselect.min.css';
+import CryptoJS from 'crypto-js';
 
 
 export default {
-  name: 'mapbankhub',
+  name: 'AddEditClientTAT',
   components: {
     Multiselect
   },
@@ -23,19 +24,20 @@ export default {
       ClientId:"",
       Bussinesstype:"",
       Client:"",
-      TAT:"",
-      bank:"",
+      tat:"",
+      type:"",
       Beneficiary:"",
       BankName:"",
-      RemittanceDay:"",
       BankAccount:"",
-      RTGS:"",
+      rtgs:"",
       AccountName:"",
       BeneficiaryName:"",
       BankName:"",
       myStr:"",
       BankAccount:"",
-      AddEditClientTAT:false
+      AddEditClientTAT:true,
+      RemittanceDay:[],
+      RemittanceDayList: []
     }
   },
 
@@ -44,12 +46,43 @@ export default {
   },
 
   mounted() {
+    var userdetailEncrypt = window.localStorage.getItem('accessuserdata')
+    var bytes             = CryptoJS.AES.decrypt(userdetailEncrypt.toString(), 'Key');
+    var plaintext         = bytes.toString(CryptoJS.enc.Utf8);
+    var userdetail        = JSON.parse(plaintext);
+    this.localuserid      = userdetail.userid;
+
     var userToken = window.localStorage.getItem('accessuserToken')
     this.myStr = userToken.replace(/"/g, '');
-    this.GetClientData()
+    this.GetClientData();
+
+    this.RemittanceDayList = [
+      {day:"Daily"},
+      {day:"Sunday"},
+      {day:"Monday"},
+      {day:"Tuesday"},
+      {day:"Wednesday"},
+      {day:"Thursday"},
+      {day:"Friday"},
+      {day:"Saturday"}
+    ];
   },
 
   methods: {
+    handleSelect(event) {
+      if (event.day == 'Daily') {
+        for (let item of this.RemittanceDayList) {
+          if (item.day != 'Daily' && this.RemittanceDay.includes(item) === false) {
+            this.RemittanceDay.push(item);
+          }
+        }
+      }
+    },
+
+    multiple(){
+      return true;
+    },
+
     GetClientBusinessConfigList(){
       if(this.ClientId.ClientMasterID == ""){
         return false;
@@ -66,12 +99,15 @@ export default {
           }
         })
         .then(result => {
-           this.ClientBusinessList = result.data.client.data;
+          this.AccountName = '';
+          this.Bussinesstype = '';
+          this.ClientBusinessList = result.data.client.data;
         }, error => {
           console.error(error)
         })
     },
-     GetClientBusinessAccounts(){
+
+    GetClientBusinessAccounts(){
        if(this.Bussinesstype == ""){
          return false;
        }
@@ -93,7 +129,8 @@ export default {
           console.error(error)
         })
     },
-     getAccountDetails(){
+
+    getAccountDetails(){
       this.input = ({
         ClientBusinessAccountId: this.AccountName
       })
@@ -106,14 +143,17 @@ export default {
           }
         })
         .then(result => {
+          if(result.data.code==200){
             this.Beneficiary = result.data.data.BeneficiaryName
             this.BankName =  result.data.data.ClientBankName
             this.BankAccount = result.data.data.ClientAccountNo
-            this.RTGS = result.data.data.ClientBankNeftIFSC
+            this.rtgs = result.data.data.ClientBankNeftIFSC
+          }
         }, error => {
           console.error(error)
         })
     },
+
     GetClientData() {
       axios({
         method: 'GET',
@@ -128,13 +168,67 @@ export default {
         console.error(error)
       })
     },
+
+    saveClientCODRemittanceData(event) {
+
+      let dData = [];
+      this.RemittanceDay.forEach(function (val) {
+        if(val.day!='Daily'){
+          dData.push(val.day);
+        }
+      });
+
+      this.input = ({
+          ClientId: this.ClientId.ClientMasterID,
+          RemittanceType: this.type,
+          RemittanceDay: dData.join(),
+          TAT: this.tat,
+          Cycle: "",
+          CustomerMailId: "",
+          IsActive: true,
+          HoldingAmount: 0,
+          CreatedBy: this.localuserid
+      })
+      axios({
+          method: 'POST',
+          'url': apiUrl.api_url + 'saveclientcodremittancedetail',
+          'data': this.input,
+          headers: {
+              'Authorization': 'Bearer '+this.myStr
+          }
+      })
+      .then((response) => {
+        if (response.data.errorCode == 0) {
+          this.$alertify.success(response.data.msg);
+          this.resetForm(event);
+        } else if (response.data.errorCode == -1) {
+          this.$alertify.error(response.data.msg)
+        }
+      })
+      .catch((httpException) => {
+          console.error('exception is:::::::::', httpException)
+      });
+    },
+
     onSubmit: function(event) {
-      this.$validator.validateAll().then(() => {
-        console.log('form is valid', this.model)
-        event.target.reset();
+      this.$validator.validateAll().then((result) => {
+        if(result){
+          this.saveClientCODRemittanceData(event);
+        }
       }).catch(() => {
         console.log('errors exist', this.errors)
       });
-    }
+    },
+
+    onSearch: function(event) {
+      this.$validator.validateAll().then((result) => {
+        console.log('resultsssss==', result);
+        if(result){
+
+        }
+      }).catch(() => {
+        console.log('errors exist', this.errors)
+      });
+    },
   }
 }
