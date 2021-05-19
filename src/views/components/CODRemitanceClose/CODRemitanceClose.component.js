ce530554f9ff0822220d854a3e9229a1acbf7662
amol.bandal <amol.bandal@xpressbees.com>
2021-05-19 16:18:25 +0530

    changes related to remittance
    Summary: changes related to remittance
    
    Test Plan: browser
    
    Reviewers: sandip.vite
    
    Differential Revision: https://phab.xbees.in/D29049

--- src/views/components/CODRemitanceClose/CODRemitanceClose.component.js
+++ src/views/components/CODRemitanceClose/CODRemitanceClose.component.js
@@ -56,7 +56,8 @@ export default {
       isActiveNow: 1,
       reportlink: '',
       totalSum: 0,
-      total: 0
+      total: 0,
+      isexport: false
     }
   },
 
@@ -163,16 +164,37 @@ export default {
         console.error(error)
       })
     },
-
+    exportreport() {
+      this.excelLoading = true;
+      this.isexport = true;
+      this.GetCODRemittanceDetailsDataTemp();
+      if (this.reportlink) {
+        window.open(this.reportlink);
+        this.excelLoading = false;
+      } else {
+        this.excelLoading = false;
+      }
+    },
     getPaginationData(pageNum) {
-      this.pageno = (pageNum - 1) * 10
+      this.pageno = (pageNum - 1) * 10;
+      this.isexport = false;
       this.GetCODRemittanceDetailsData();
     },
-
     GetCODRemittanceDetailsData(event) {
+      this.isexport = false;
+      this.GetCODRemittanceDetailsDataTemp(event);
+    },
+
+    GetCODRemittanceDetailsDataTemp(event) {
 
       if (this.selected) {
-        if (this.fromDate > this.toDate) {
+        let diffTime = Math.abs(new Date(this.toDate) - new Date(this.fromDate));
+        let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+        if (diffDays > 30) {
+          document.getElementById("fdate").innerHTML = "Difference between From date & To date should not be greater than 30 days.";
+          return false;
+        } else if (this.fromDate > this.toDate) {
           document.getElementById("fdate").innerHTML = "From date should not be greater than To date.";
           return false;
         } else {
@@ -205,6 +227,7 @@ export default {
         this.input.ClientId = cData
       }
 
+      this.input.isexport = this.isexport;
 
       if (this.fromDate) {
         this.input.TransactionFromDate = this.fromDate;
@@ -213,7 +236,8 @@ export default {
       if (this.toDate) {
         this.input.TransactionToDate = this.toDate;
       }
-
+      this.input.offset = this.pageno;
+      this.input.limit = 10;
 
       this.isLoading = true;
       axios({
@@ -230,6 +254,7 @@ export default {
           this.listCODRemitanceData = [];
 
           if (result.data.code == 200) {
+
             this.listCODRemitanceData = result.data.remittanceArr;
             this.isLoading = false;
 
@@ -239,6 +264,19 @@ export default {
               this.pagecount = 1
             } else {
               this.pagecount = Math.ceil(totalRows / 10)
+            }
+            this.exportf = true;
+            /**
+             * excel download
+             * @param  {[type]} this [description]
+             * @return {[type]}      [description]
+             */
+            if (this.isexport === true) {
+              if (result.data.remittanceArr.length > 0) {
+                this.getDownloadCsvObject(result.data.remittanceArr);
+              } else {
+                this.$alertify.error('Sorry..! no record found for excel download.');
+              }
             }
             // this.exportCODRemittanceDetailsData();
           } else {
@@ -250,9 +288,64 @@ export default {
           this.$alertify.error('Error Occured');
         })
     },
+    /**
+     * download csv file
+     * @param  {[type]} csvData [description]
+     * @return {[type]}         [description]
+     */
+    getDownloadCsvObject(csvData) {
+      var today = new Date();
+      var dd = today.getDate();
+      var mm = today.getMonth() + 1;
+      var yyyy = today.getFullYear();
+      var today = dd + "" + mm + "" + yyyy;
+      var data, filename, link;
+      filename = "COD_Close_Remitance" + today + ".csv";
+      var csv = this.convertArrayOfObjectsToCSV({
+        data: csvData
+      });
+      if (csv == null) return;
+      filename = filename || "export.csv";
+      if (!csv.match(/^data:text\/csv/i)) {
+        csv = "data:text/csv;charset=utf-8," + csv;
+      }
+      data = encodeURI(csv);
+      link = document.createElement("a");
+      link.setAttribute("href", data);
+      link.setAttribute("download", filename);
+      document.body.appendChild(link);
+      link.click();
+      // link.removeChild(link);
+    },
+    convertArrayOfObjectsToCSV: function(args) {
+      var result, ctr, keys, columnDelimiter, lineDelimiter, data;
+      data = args.data || null;
+      if (data == null || !data.length) {
+        return null;
+      }
+      columnDelimiter = args.columnDelimiter || ",";
+      lineDelimiter = args.lineDelimiter || "\n";
+      keys = Object.keys(data[0]).slice(0);
+      result = "";
+      result += keys.join(columnDelimiter);
+      result += lineDelimiter;
+      data.forEach(function(item) {
+        ctr = 0;
+        keys.forEach(function(key) {
+          if (ctr > 0) result += columnDelimiter;
+          if (item[key] != null) {
+            result += '"' + item[key] + '"';
+          }
+          ctr++;
+        });
+        result += lineDelimiter;
+      });
+      this.excelLoading = false;
+      return result;
+    },
     getPaginationDataDate(pageNum) {
-
-      this.pageno = (pageNum - 1) * 10
+      this.pageno = (pageNum - 1) * 10;
+      this.isexport = false;
       this.GetCODRemittanceDetailsDataDate();
 
     },
@@ -380,15 +473,7 @@ export default {
         })
     },
 
-    exportreport() {
-      this.excelLoading = true;
-      if (this.reportlink) {
-        window.open(this.reportlink);
-        this.excelLoading = false;
-      } else {
-        this.excelLoading = false;
-      }
-    },
+
 
     /** getDownloadCsvObject(csvData) {
       var today   = new Date();
@@ -471,11 +556,13 @@ export default {
     resetForm() {
       this.fromDate = this.toDate = '';
       this.ClientId = "";
+      document.getElementById("fdate").innerHTML = "";
       $("input[value='TransactionDate']").attr("checked", true);
       this.pageno = this.resultCount = 0;
       this.listCODRemitanceData = [];
       this.listCODRemitanceDataDate = [];
       this.exportf = false;
+      this.isexport = false;
       this.$validator.reset();
       this.errors.clear();
     },
