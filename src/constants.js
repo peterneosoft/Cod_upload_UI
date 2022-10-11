2e6b61734ea24deef7749a36cdde4e728b335e56
amol.bandal <amol.bandal@xpressbees.com>
2022-10-11 15:37:30 +0530

    changes in constant

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export default {
 
 		api_url: (process.env.NODE_ENV==='production')? 'http://codmanagementapi.xbees.in/api/':'http://codstageapi.xbees.in/api/',
-		iptracker_url:(process.env.NODE_ENV==='production') ?'http://track.xbees.in/api/':'http://stageiptracking.xbees.in/api/'
+		iptracker_url:(process.env.NODE_ENV==='production') ?'http://track.xbees.in/api/UserTracker':'http://stageiptracking.xbees.in/api/UserTracker'
 }
