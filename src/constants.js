3dea4ec7691cecb745066597f2e2ced72c367663
amol.bandal <amol.bandal@xpressbees.com>
2022-12-09 14:56:50 +0530

    changes in constant for https

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export default {
 
-		api_url: (process.env.NODE_ENV==='production')? 'http://codmanagementapi.xbees.in/api/':location.protocol+'//codstageapi.xbees.in/api/',		
+		api_url: (process.env.NODE_ENV==='production')? location.protocol+'//codmanagementapi.xbees.in/api/':location.protocol+'//codstageapi.xbees.in/api/',		
 		iptracker_url:(process.env.NODE_ENV==='production') ?'http://track.xbees.in/api/UserTracker':'http://stageiptracking.xbees.in/api/UserTracker',
 		forgotpwd_url:(process.env.NODE_ENV==='production') ?'http://userauthentication.xbees.in/#/forgotpassword':'http://stageusermanagement.xbees.in/#/forgotpassword'
 }
