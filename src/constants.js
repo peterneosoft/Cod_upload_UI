3d714c3798bcd335b0b0dbfb183cab74ecdd94d0
amol.bandal <amol.bandal@xpressbees.com>
2023-03-15 17:13:05 +0530

    changes in iptracker call

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export default {
 
 		api_url: (process.env.NODE_ENV==='production')? location.protocol+'//codmanagementapi.xbees.in/api/':location.protocol+'//codstageapi.xbees.in/api/',		
-		iptracker_url:(process.env.NODE_ENV==='production') ?'http://track.xbees.in/api/UserTracker':'http://stageiptracking.xbees.in/api/UserTracker',
-		forgotpwd_url:(process.env.NODE_ENV==='production') ?'http://userauthentication.xbees.in/#/forgotpassword':'http://stageusermanagement.xbees.in/#/forgotpassword'
+		iptracker_url:(process.env.NODE_ENV==='production') ? location.protocol+'//track.xbees.in/api/UserTracker':location.protocol+'//stageiptracking.xbees.in/api/UserTracker',
+		forgotpwd_url:(process.env.NODE_ENV==='production') ? location.protocol+'//userauthentication.xbees.in/#/forgotpassword':location.protocol+'//stageusermanagement.xbees.in/#/forgotpassword'
 }
