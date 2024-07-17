f85eb542e70cf556d69fb9425084b4fec0a5b792
peterneosoft <peter.khalko@neosoftmail.com>
2024-07-17 12:41:48 +0530

    updated connection

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export default {
 
-		api_url: (process.env.NODE_ENV==='production')? location.protocol+'//codmanagementapi.xbees.in/api/':location.protocol+'//codstageapi.xbees.in/api/',		
-		iptracker_url:(process.env.NODE_ENV==='production') ? location.protocol+'//track.xbees.in/api/UserTracker':location.protocol+'//stageiptracking.xbees.in/api/UserTracker',
-		forgotpwd_url:(process.env.NODE_ENV==='production') ? location.protocol+'//userauthentication.xbees.in/#/forgotpassword':location.protocol+'//stageusermanagement.xbees.in/#/forgotpassword'
-}
+      api_url: (process.env.NODE_ENV==='production')? location.protocol+'//codmanagementapi.xbees.in/api/': 'http:'+'//localhost:4500/api/',       
+     iptracker_url:(process.env.NODE_ENV==='production') ? location.protocol+'//track.xbees.in/api/UserTracker':'https:'+'//stageiptracking.xbees.in/api/UserTracker',
+     forgotpwd_url:(process.env.NODE_ENV==='production') ? location.protocol+'//userauthentication.xbees.in/#/forgotpassword':'https:'+'//stageusermanagement.xbees.in/#/forgotpassword'
+	}
